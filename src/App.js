bdd3c1f23345341107d72567dd81f14b213428b6
shankey raheja <shankeyraheja@gmail.com>
2024-08-05 18:07:56 +1200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { GoogleMap, useJsApiLoader, StandaloneSearchBox} from '@react-google-maps/api';
+import { useRef } from 'react';
 
 function App() {
+  const inputref = useRef(null)
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.REACT_APP_GOOGLEMAPS_API_KEY,
+    libraries:["places"]
+  })
+
+  console.log(isLoaded)
+
+  const handleOnPlacesChanged = () => {
+    let address = inputref.current.getPlaces()
+    console.log("address", address)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{marginTop:"10%", textAlign:"center"}}>
+      {isLoaded && 
+      <StandaloneSearchBox
+        onLoad={(ref) => inputref.current = ref}
+        onPlacesChanged={handleOnPlacesChanged}
+      >
+        <input
+          type="text"
+          placeholder="Start typing your address"
+          style={{
+            boxSizing: `border-box`,
+            border: `1px solid transparent`,
+            width: `50%`,
+            height: `50px`,
+            padding: `0 12px`,
+            borderRadius: `3px`,
+            boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+            fontSize: `14px`,
+            outline: `none`,
+            textOverflow: `ellipses`,
+            marginTop: "30px",
+          }}
+        />
+        </StandaloneSearchBox>
+        }
     </div>
   );
 }
